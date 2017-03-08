f37cf2f72785dcb7084299edcff3f9c6a86f1469
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-08 16:46:21 -0600

    finished assignment

--- group_01.js
+++ group_01.js
@@ -18,56 +18,82 @@ function convertEmployees(array) {
 function newEmployee(employeeArray){
   var resultArray = [];
   //constructor for new employee array
- resultArray.push(employeeArray[0]);
- resultArray.push(bonusPercent());
-  // this.annualcomp = annualComp;
+ resultArray[0] = (employeeArray[0]);
+
+ //resultArray[2] = annualComp;
   // this.totalbonus = totalBonus;
+
   //Figures out employee bonus percentage
-
-  function bonusPercent(){
-    var employeeBonus = 0;
-    if(employeeArray[3]==5){
-      employeeBonus += '.10';
-    } else if (employeeArray[3]==3) {
-      employeeBonus += '.04';
-    } else if (employeeArray[3]==4) {
-      employeeBonus += '.06';
-    } else {
-      employeeBonus += 0;
-    }
-
-      function bonusAdded(){
-        if(employeeArray[1].length==4){
-      employeeBonus += '.05';
-    }
-  }
-    if(employeeArray[2]>65000)
-    {
-      employeeBonus -= '.01';
-    }
-
-    if(employeeBonus > '.13')
-    {
-      employeeBonus = '.13';
-    }
-    else if (employeeBonus < 0) {
+  function percent(){
+    var employeeBonus = employeeArray[3];
+    switch (employeeBonus){
+    case 1:
       employeeBonus = 0;
+    break;
+    case 2:
+      employeeBonus = 0;
+    break;
+    case 3:
+      employeeBonus = 0.04;
+    break;
+    case 4:
+      employeeBonus = 0.06;
+    break;
+    case 5:
+      employeeBonus = 0.10;
+    break;
     }
+    //console.log(employeeBonus);
     return employeeBonus;
-
+  }
+//adds bonus based on length of employee number
+  function added(){
+    var timeBonus = 0;
+    if(employeeArray[1].length==4){
+      timeBonus = 0.05;
+    } else {
+      timeBonus = 0;
+    }
+    return timeBonus;
   }
 
 
+//limits bonus based on employee salary
+  function limit(){
+    if(employeeArray[2]>65000){
+      return -0.01;
+    } else {
+      return 0;
+    }
+  }
+
+//limits employee bonus to .13
+    function bonusThreshold(){
+    var bonusPercent = percent()+added()+limit();
+      if(bonusPercent > 0.13)
+    {
+      bonusPercent = 0.13;
+    } else if (bonusPercent<0){
+      bonusPercent = 0;
+    }
+    return bonusPercent;
+  }
+var bonusPercent = bonusThreshold();
+resultArray[1] = (bonusPercent);
 //figures out the total bonus
-  var totalBonus = function totalBonus(){
-    var totalBonus = parsInt(employeeBonus*employeeArray[2]);
-    return totalBonus;
-  };
+    function totalBonus(){
+      var totalBonus = parseInt(bonusPercent*employeeArray[2]);
+      return totalBonus;
+    }
+    var bonus = totalBonus();
+  resultArray[3] = (bonus);
 
 //figures out annual compensation
-  var annualComp = function totalComp(){
-    var total = (totalBonus+employeeArray[2]);
-  };
-
+  function totalComp(){
+    var total = bonus + parseInt(employeeArray[2]);
+    return total;
+  }
+  var totalSTI = totalComp();
+resultArray[2] = totalSTI;
   return resultArray;
 }
